11ae23b821575c91d89d471499c62928764a9960
Aditya Singh <118333811+s4kenn@users.noreply.github.com>
2024-09-15 22:01:19 +0530

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,33 @@
 fun main() {
-    println("Hello World");
+    println("Hello World")
+    // String Interpolation
+    val myAge = 10
+    println("My age is ${myAge}")
+    /*
+    Kotlin is used for:
+
+    Mobile applications (specially Android apps)
+    Web development
+    Server side applications
+    Data science
+     */
+
+    /*
+    Why Use Kotlin?
+    Kotlin is fully compatible with Java
+    Kotlin works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc.)
+    Kotlin is concise and safe
+    Kotlin is easy to learn, especially if you already know Java
+    Kotlin is free to use
+     */
+    /*
+    Before Kotlin version 1.3, it was required to use the main() function with parameters,like:
+    fun main(args : Array<String>)
+     */
+    // Kotlin Output
+    println("Hello Output")
+    print("Hello Output \n") // Doesn't insert a new line. We need to do it explicitly
+    var newVar1: String = "Aditya"
+    val newVar2: Int = 10
+    println("${newVar2} and ${newVar1}")
 }
